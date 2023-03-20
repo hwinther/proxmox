a229b26fec520336f14ef63ef14b14614334c3a7
Hans Christian Winther-Sorensen <hc@wsh.no>
2023-03-20 19:04:18 +0100

    Reformat of ddns.py

--- scripts/ddns.py
+++ scripts/ddns.py
@@ -39,40 +39,40 @@ filehandle = urllib.request.urlopen(url)
 data = filehandle.read().decode('utf-8')
 ip = data.split(': ')[1].split('<')[0]
 if verbose:
-  print('External IP found: %s' % ip)
+    print('External IP found: %s' % ip)
 
 resolver = dns.resolver.Resolver()
 resolver.nameservers = [nsserver]
 try:
-    answer = resolver.resolve(ddns_domain+'.', 'A')
+    answer = resolver.resolve(ddns_domain + '.', 'A')
 except dns.resolver.NXDOMAIN:
     answer = None
 
 if answer != None:
-  resolved_ip = answer.rrset[0].to_text()
-  if resolved_ip == ip:
-    if verbose:
-      print('DNS record matches current IP, not updating')
-    sys.exit(0)
+    resolved_ip = answer.rrset[0].to_text()
+    if resolved_ip == ip:
+        if verbose:
+            print('DNS record matches current IP, not updating')
+        sys.exit(0)
 
 nsupdate_script_path = '/tmp/nsupdate.script'
-nsupdate_script = 'server %s\n'\
-'zone %s\n'\
-'update delete %s. A\n'\
-'update add %s. 3600 A %s\n'\
-'show\n'\
-'send\n' % (nsserver, zone_name, ddns_domain, ddns_domain, ip)
+nsupdate_script = 'server %s\n' \
+                  'zone %s\n' \
+                  'update delete %s. A\n' \
+                  'update add %s. 3600 A %s\n' \
+                  'show\n' \
+                  'send\n' % (nsserver, zone_name, ddns_domain, ddns_domain, ip)
 
 if verbose:
-  print('Writing to script file %s, content:' % nsupdate_script_path)
-  print(nsupdate_script)
+    print('Writing to script file %s, content:' % nsupdate_script_path)
+    print(nsupdate_script)
 open(nsupdate_script_path, 'w').write(nsupdate_script)
 
-print('Updating A record for %s to %s' %(ddns_domain, ip))
+print('Updating A record for %s to %s' % (ddns_domain, ip))
 if ddns_key_path is None:
-  cmd = '/usr/bin/nsupdate -y hmac-md5:%s:%s %s' % (ddns_key_name, ddns_key_secret, nsupdate_script_path)
+    cmd = '/usr/bin/nsupdate -y hmac-md5:%s:%s %s' % (ddns_key_name, ddns_key_secret, nsupdate_script_path)
 else:
-  cmd = '/usr/bin/nsupdate -k %s %s' % (ddns_key_path, nsupdate_script_path)
+    cmd = '/usr/bin/nsupdate -k %s %s' % (ddns_key_path, nsupdate_script_path)
 if verbose:
-  print('Executing: %s' % cmd)
+    print('Executing: %s' % cmd)
 os.system(cmd)
