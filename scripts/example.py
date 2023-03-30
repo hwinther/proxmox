091311e4f3808f3fc3ce469a3e78fd488e497130
Hans Christian Winther-Sorensen <hc@wsh.no>
2023-03-31 00:33:15 +0200

    Added bind authoritative support

--- scripts/example.py
+++ scripts/example.py
@@ -1,6 +1,6 @@
 from src.lxc.actions import update_lxc_templates
 from src.lxc.distro.alpine.actions import AlpineContainer
-from src.lxc.distro.alpine.services.bind import install_bind_dns_recursive
+from src.lxc.distro.alpine.services.bind import MasterZone, install_bind_dns_authoritative, install_bind_dns_recursive
 from src.lxc.distro.alpine.services.dhcpd import install_isc_dhcpd
 from src.lxc.distro.alpine.services.gateway import install_gateway_nat
 from src.lxc.models import NetworkInterface, Subnet
@@ -10,6 +10,22 @@ def main():
     alpine_newest_image_name = update_lxc_templates()
     # TODO: translate image names not just for local (template_)storage?
     image_path = f'/var/lib/vz/template/cache/{alpine_newest_image_name}'
+
+    dns_master = AlpineContainer(605)
+    dns_master.purge_container()
+    dns_master.create_container('dns-test2',
+                                image_path,
+                                [NetworkInterface(vlan_tag=100,
+                                                  ip4='10.100.0.3/24',
+                                                  gw4='10.100.0.1')],
+                                onboot=1)
+    dns_master.update_container()
+    print(dns_master.get_ip(0))
+    install_bind_dns_authoritative(dns_master,
+                                   dns_master.network_interfaces[0],
+                                   master_zones=[MasterZone(domain_name='test.lan', slaves=[])])
+    
+    return
 
     # Create NAT gateway
     nat_gateway = AlpineContainer(601)
